f8f46eaab511d2489640e1a6d1ba9671d2807014
fatgenius <dbtlive@gmail.com>
2017-05-02 14:31:48 +0200

    add the  database add the table

---
+++ src/main/java/sqlite/CreateTable.java
@@ -0,0 +1,39 @@
+package sqlite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.*;
+/**
+ * Created by Gebruiker on 02/05/2017.
+ */
+public class CreateTable {
+
+
+        public static void main( String args[] )
+        {
+            Connection c = null;
+            Statement stmt = null;
+            try {
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                System.out.println("Opened database successfully");
+
+                stmt = c.createStatement();
+                String sql = "CREATE TABLE COMPANY " +
+                        "(ID INT PRIMARY KEY     NOT NULL," +
+                        " NAME           TEXT    NOT NULL, " +
+                        " AGE            INT     NOT NULL, " +
+                        " DATE        CHAR(50), " +
+                        " BPM         REAL)";
+                stmt.executeUpdate(sql);
+                stmt.close();
+                c.close();
+            } catch ( Exception e ) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                System.exit(0);
+            }
+            System.out.println("Table created successfully");
+        }
+    }
+
